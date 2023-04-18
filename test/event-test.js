686fc67f6538db34caa7787464908c64e928c9f7
Adam Meza <Adammeza95@gmail.com>
2023-04-18 14:51:59 -0400

    test remote git

---
+++ test/event-test.js
@@ -0,0 +1,28 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe("Test Suite", function () {
+  beforeEach(function () {
+    // Set up code before each test
+  });
+
+  it("Test description 1", function () {
+    // Test code for the first case
+  });
+
+  it.skip("Test description 2", function () {
+    // Test code for the second case
+  });
+
+  it("Test description 3", function () {
+    // Test code for the third case
+  });
+
+  it.skip("Test description 4", function () {
+    // Test code for the fourth case
+  });
+
+  it("Test description 5", function () {
+    // Test code for the fifth case
+  });
+});
